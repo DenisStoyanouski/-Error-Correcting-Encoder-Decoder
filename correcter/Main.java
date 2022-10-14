b53bfcc72190e4bdc9a036bcfc68164463c95a79
Denis Stoyanouski <vandenst@tut.by>
2022-10-14 20:03:03 +0300

    added methods emulateError, addExtra, generateRandomChar;

--- correcter/Main.java
+++ correcter/Main.java
@@ -1,5 +1,6 @@
 package correcter;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +9,26 @@ public class Main {
     }
 
     public static void runProgram() {
-        getInput();
+        char[] phrase = getInput().toCharArray();
+        System.out.println(emulateError(phrase));
+    }
+
+    private static String emulateError(char[] phrase) {
+
+        for (int i = 0; i < phrase.length; i += 2) {
+            phrase[i + addExtra()] =  generateRandomChar();
+        }
+
+
+        return String.valueOf(phrase);
+    }
+
+    private static char generateRandomChar() {
+    }
+
+    private static int addExtra() {
+        Random random = new Random();
+        return random.nextInt(3);
     }
 
     private static String getInput() {
