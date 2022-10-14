802c9674d6328becab5c061567c30f3fb36ff294
Denis Stoyanouski <vandenst@tut.by>
2022-10-14 19:44:44 +0300

    add method getInput

--- correcter/Main.java
+++ correcter/Main.java
@@ -1,7 +1,20 @@
 package correcter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        runProgram();
+    }
+
+    public static void runProgram() {
+        getInput();
+    }
+
+    private static String getInput() {
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        return input;
     }
 }
