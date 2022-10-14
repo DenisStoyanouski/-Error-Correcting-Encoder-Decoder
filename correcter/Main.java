9e323324b3eff43bc6c1ceed0bf92adb3b457bff
Denis Stoyanouski <vandenst@tut.by>
2022-10-14 20:14:47 +0300

    added methods generateRandomChar;

--- correcter/Main.java
+++ correcter/Main.java
@@ -13,17 +13,19 @@ public class Main {
         System.out.println(emulateError(phrase));
     }
 
+    private static String getInput() {
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        return input;
+    }
+
     private static String emulateError(char[] phrase) {
 
         for (int i = 0; i < phrase.length; i += 2) {
             phrase[i + addExtra()] =  generateRandomChar();
         }
-
-
         return String.valueOf(phrase);
-    }
-
-    private static char generateRandomChar() {
     }
 
     private static int addExtra() {
@@ -31,10 +33,15 @@ public class Main {
         return random.nextInt(3);
     }
 
-    private static String getInput() {
-        String input;
-        Scanner scanner = new Scanner(System.in);
-        input = scanner.nextLine();
-        return input;
+    private static char generateRandomChar() {
+        char symbol;
+        int byteCode;
+        Random random = new Random();
+        byteCode = random.nextInt(122);
+        symbol = (char) byteCode;
+        if (!String.valueOf(symbol).matches("[\\s\\w]")) {
+            generateRandomChar();
+        }
+        return symbol;
     }
 }
